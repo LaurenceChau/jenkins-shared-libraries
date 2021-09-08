4a5c19833f05f6d7dbcb3a5121fa94302e2fbda9
laurencechau <laurence_chau@hotmail.com>
2021-09-09 01:28:14 +0800

    update

--- vars/stage_deploy.groovy
+++ vars/stage_deploy.groovy
@@ -1,16 +1,16 @@
 def call(){
 
-    script {
-        def tests = ["file-1", "file-2", "file-3"]
-        for (f in tests) {
-            tests["${f}"] = {
 
-                stage("${f}") {
-                    echo '${f}'
-                }
+    def tests = ["file-1", "file-2", "file-3"]
+    for (f in tests) {
+        tests["${f}"] = {
+
+            stage("${f}") {
+                echo '${f}'
             }
         }
-        parallel tests
     }
+    parallel tests
+
     
 }
