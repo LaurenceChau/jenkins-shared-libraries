6316ac3e16235717527cad71850a2635588b95fb
laurencechau <laurence_chau@hotmail.com>
2021-09-09 02:13:32 +0800

    update

--- vars/stage_p3.groovy
+++ vars/stage_p3.groovy
@@ -5,7 +5,7 @@ def call(){
             message "Should we continue?"
             ok "Yes, we should."
             parameters {
-            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+                string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
             }
         }
         echo "Hello, ${PERSON}"
