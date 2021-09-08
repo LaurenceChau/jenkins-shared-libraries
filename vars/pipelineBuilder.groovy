2b97b73226c3c1cbfc7def6a283adad47737d8be
laurencechau <laurence_chau@hotmail.com>
2021-09-09 01:49:31 +0800

    update

--- vars/pipelineBuilder.groovy
+++ vars/pipelineBuilder.groovy
@@ -4,14 +4,16 @@ def call(body) {
     body.delegate = config
     body()
     
-    files = ["file-1", "file-2", "file-3"]
+    def files = ["file-1", "file-2", "file-3"]
+    def pipeline_name = "p1"
     pipeline {
         agent any
         stages {
             stage('1') {
                 steps {
                     script {
-                        parallel stage_deploy()
+                        if ( pipeline_name == "p1") parallel stage_deploy()
+                        else if ( pipeline_name == "p2" ) 
                     }
                     
                 }
