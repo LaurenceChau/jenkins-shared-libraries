71b5297e64aade42f00b1f84fdfcdd406fd70e97
laurencechau <laurence_chau@hotmail.com>
2021-09-09 02:10:49 +0800

    update

---
+++ vars/stage_p3.groovy
@@ -0,0 +1,15 @@
+def call(){
+
+    node {
+        input {
+            message "Should we continue?"
+            ok "Yes, we should."
+            parameters {
+            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+            }
+        }
+        echo "Hello, ${PERSON}"
+    }
+
+    
+}
